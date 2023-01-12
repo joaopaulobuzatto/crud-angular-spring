7558cf640eb8ff58c6c6280475938d6e83a0cb79
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2023-01-12 11:56:02 -0300

    Criado dialog para exibicao de mensagem de erro

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
 
-import {Observable} from 'rxjs';
+import {catchError, Observable, of} from 'rxjs';
 import {Course} from '../model/course';
 import {CoursesService} from '../services/courses.service';
+import {ErrorDialogComponent} from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -14,11 +16,24 @@ export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private coursesService: CoursesService) {
-    this.courses$ = this.coursesService.list();
+  constructor(private coursesService: CoursesService,
+              public dialog: MatDialog) {
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.');
+          return of([]);
+        })
+      );
   }
 
   ngOnInit(): void {
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
 }
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-import {first, tap} from 'rxjs';
+import {delay, first, tap} from 'rxjs';
 import {Course} from '../model/course';
 
 @Injectable({
@@ -18,6 +18,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
       .pipe(
         first(),
+        delay(5000),
         tap(courses => console.log(courses))
       );
   }
