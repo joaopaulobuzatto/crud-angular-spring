c2b93a04f6adc7fa294283c8cb58e85f69f541e9
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2023-01-13 12:16:25 -0300

    Criado formulario para criar curso

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {CoursesRoutingModule} from './courses-routing.module';
 import {CoursesComponent} from './courses/courses.component';
 import {AppMaterialModule} from '../shared/app-material/app-material.module';
 import {SharedModule} from '../shared/shared.module';
-import { CourseFormComponent } from './course-form/course-form.component';
+import {CourseFormComponent} from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CourseFormComponent } from './course-form/course-form.component';
     CommonModule,
     CoursesRoutingModule,
     AppMaterialModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class CoursesModule {
