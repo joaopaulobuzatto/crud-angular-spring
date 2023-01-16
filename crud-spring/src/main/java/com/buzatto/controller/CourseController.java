8600cb8f0fb09c605a8691b745e947760ba3d4a4
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2023-01-16 16:16:07 -0300

    Validações da API com Java Bean Validation

--- crud-spring/src/main/java/com/buzatto/controller/CourseController.java
+++ crud-spring/src/main/java/com/buzatto/controller/CourseController.java
@@ -2,13 +2,18 @@ package com.buzatto.controller;
 
 import com.buzatto.model.Course;
 import com.buzatto.service.CourseService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping(path = "/api/courses")
 @AllArgsConstructor
@@ -22,27 +27,27 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id) {
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
         return courseService.findById(id)
                 .map(recordFound -> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course) {
+    public ResponseEntity<Course> create(@RequestBody @Valid Course course) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(courseService.save(course));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
         return courseService.update(id, course)
                 .map(courseUpdated -> ResponseEntity.ok().body(courseUpdated))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
         return courseService.findById(id)
                 .map(recordFound -> {
                     courseService.delete(id);
--- crud-spring/src/main/java/com/buzatto/model/Course.java
+++ crud-spring/src/main/java/com/buzatto/model/Course.java
@@ -2,7 +2,11 @@ package com.buzatto.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
@@ -13,9 +17,15 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 3, max = 100)
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "back-end|front-end")
     @Column(length = 10, nullable = false)
     private String category;
 }
