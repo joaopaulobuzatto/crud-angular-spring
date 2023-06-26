7b4da66e87582808f21cfdb180dd57b932d8a3f2
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2023-06-26 09:46:24 -0300

    Add and configure springdoc-openapi-starter-webmvc-ui

---
+++ crud-spring/src/main/java/com/buzatto/config/SpringdocConfig.java
@@ -0,0 +1,27 @@
+package com.buzatto.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("CRUD-Spring API")
+                        .description("CRUD Spring example application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("CRUD-Spring Wiki Documentation")
+                        .url("https://spring.wiki.github.org/docs"));
+    }
+
+}
