e17dd6f80d0e9fe53f55351f17bf7eb909609647
Joao Paulo Buzatto <joaopaulobuzatto@gmail.com>
2023-06-26 10:31:34 -0300

    Open API config

--- crud-spring/src/main/java/com/buzatto/config/OpenAPIConfig.java
+++ crud-spring/src/main/java/com/buzatto/config/OpenAPIConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
-public class SpringdocConfig {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI springShopOpenAPI() {
